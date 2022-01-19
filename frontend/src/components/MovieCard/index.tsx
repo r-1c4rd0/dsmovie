cdc4c7beb62f0b9d753c6429084dc41dedd0fe8b
Ricardo Santiago <ricardosantiagocwb@gmail.com>
2022-01-18 22:26:07 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,11 +16,9 @@ function MovieCard({ movie }: Props) {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
-
             </div>
         </div>
     );
